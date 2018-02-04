41d2ef4761b3b06d9d3f7fbcb3eb8e369b057f9f
William Hoy <willi@DESKTOP-8JUUS67>
2018-02-04 14:15:48 -0600

    d

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  componentDidMount() {
+    axios.get("/api/test").then(
+      res => console.log(res)
+      );
+  }
   render() {
     return (
       <div className="App">
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
-
-app.get("/test", function(req, res) {
-	res.send("all good!");
-})
-
 if(process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
+
+
+app.get("/api/test", function(req, res) {
+	return res.json("all good!");
+})
+
 
 app.get("*", function(req, res) {
 	res.sendFile(path.join(__dirname, "./client/build/index.html"));
