715e5dbfeae2621ba2426f1dac780ba9c0a5c068
William Hoy <willi@DESKTOP-8JUUS67>
2018-02-04 13:31:05 -0600

    deploy

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+
 app.get("/test", function(req, res) {
 	res.send("all good!");
+})
+
+if(process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
+
+app.get("*", function(req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 
 
